9916562652fd5efeac05dc299ec7c80cd4f42764
gguevara112 <guguevara@alumnos.uai.cl>
2024-12-06 20:20:25 -0300

    spinner fix on product details

--- src/componentsDBOFF/ProductDetailsOPF.jsx
+++ src/componentsDBOFF/ProductDetailsOPF.jsx
@@ -34,25 +34,38 @@ const ProductDetailsOPF = () => {
     }
   }, []);
 
+  if (loading) {
+    return <div className="spinnert"></div>; // Muestra el spinner mientras se carga
+  }
 
   if (!product) return <p>{t('MnOpQrStUvWx15')}</p>; // Producto no encontrado
-  
 
-  return ( 
+  return (
     <div className="product-details">
       <div className="product-image">
         <img src={product.image_url} alt={product.product_name} />
       </div>
       <div className="product-info">
         <h2>{product.product_name}</h2>
-        <p><strong>{t('MnOpQrStUvWx18')}:</strong> {product.brands}</p> {/* Marca */}
-        <p><strong>{t('MnOpQrStUvWx19')}:</strong> {product.code}</p> {/* Código de barras */}
-        <p className="ingredients"><strong>{t('MnOpQrStUvWx20')}:</strong> {product.ingredients_text || t('MnOpQrStUvWx21')}</p> {/* Ingredientes */}
-        <p><strong>{t('MnOpQrStUvWx22')}:</strong> {localStorage.getItem('selectedProductId')}</p> {/* ID del producto */}
+        <p>
+          <strong>{t('MnOpQrStUvWx18')}:</strong> {product.brands}
+        </p>{' '}
+        {/* Marca */}
+        <p>
+          <strong>{t('MnOpQrStUvWx19')}:</strong> {product.code}
+        </p>{' '}
+        {/* Código de barras */}
+        <p className="ingredients">
+          <strong>{t('MnOpQrStUvWx20')}:</strong> {product.ingredients_text || t('MnOpQrStUvWx21')}
+        </p>{' '}
+        {/* Ingredientes */}
+        <p>
+          <strong>{t('MnOpQrStUvWx22')}:</strong> {localStorage.getItem('selectedProductId')}
+        </p>{' '}
+        {/* ID del producto */}
       </div>
     </div>
   );
-  
 };
 
 export default ProductDetailsOPF;
